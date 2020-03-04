8591c65ab5329cd8e146bd1a23c5397c0708e314
Joe <jharrison@sales-i.com>
2020-03-04 21:58:06 +0000

    renaming widgets folders

---
+++ src/components/feature/widgets/colorful/cards/profileImageList/BlockTitle.vue
@@ -0,0 +1,23 @@
+<template>
+  <styled-title>{{title}}</styled-title>
+</template>
+
+<script>
+import styled from "vue-styled-components";
+
+const StyledTitle = styled.div`
+  font-size: 14px;
+  text-align: center;
+  color: ${props => props.theme.colorSubtitle};
+  margin-bottom: 0.85rem;
+`;
+
+export default {
+  props: {
+    title: String
+  },
+  components: {
+    StyledTitle
+  }
+};
+</script>

60d5c579480ed469862f061cd42d53c9ff35fb7b
Joe Harrison <joeh@low6.co.uk>
2020-12-05 16:15:02 +0000

    fixing api

--- api/routes/dashboards/designer/route.js
+++ api/routes/dashboards/designer/route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  const json = require("./data");
+  const json = require("./data.json");
 
   console.log("designer", json);
 

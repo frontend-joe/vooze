30be2996ef19da4ecdcf816b697c590ff465cb56
Joe Harrison <joeh@low6.co.uk>
2020-12-05 16:08:01 +0000

    fixing api

--- api/my_functions/hello-background.js
+++ api/my_functions/hello-background.js
@@ -1,5 +1,5 @@
 "use strict";
 
-const app = require("../server");
+const app = require("../server.js");
 
 app.listen(3000, () => console.log("Local app listening on port 3000!"));
--- api/server-local.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-const app = require("./server");
-
-app.listen(3000, () => console.log("Local app listening on port 3000!"));
